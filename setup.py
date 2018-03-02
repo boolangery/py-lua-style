67262a8937b5ed9361853cda75667c5b4709340c
boolangery <eliott.dumeix@gmail.com>
2018-03-02 17:22:19 +0100

    use luaparser 1.2.0.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6'
     ],
-    install_requires=['luaparser<=1.1.6'],
+    install_requires=['luaparser<=1.2.0'],
     entry_points={
         'console_scripts': [
             'luastyle = luastyle.__main__:main'
